9a8bb38fb4ea8ec21efe3dd7ba28f547e78c875e
Jonathan Yan <jonathan.yan10@googlemail.com>
2024-09-08 16:42:17 +0100

    Added ticket details as url extention and nav

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-        revalidate: 3600 // use 0 to not cache data and have it refresh every time
-    }
+      revalidate: 3600, // use 0 to not cache data and have it refresh every time
+    },
   });
 
   return res.json();
@@ -15,18 +16,20 @@ export default async function TicketList() {
 
   return (
     <>
-    {tickets.map(ticket => (
+      {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0,200)}...</p>
+            <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
-                {ticket.priority} priority
+              {ticket.priority} priority
             </div>
+          </Link>
         </div>
-    ))}
-    {tickets.length === 0 && (
+      ))}
+      {tickets.length === 0 && (
         <p className="text-center">There are no open tickets</p>
-    )}
+      )}
     </>
   );
 }
